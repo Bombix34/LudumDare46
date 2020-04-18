900a2ce32c0d02c00354ae92447598ba4b585869
Hugo <bombix34@gmail.com>
2020-04-18 19:12:39 +0200

    Rough FPS controller

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -9.81f;
+
+    public float jumpHeight = 1f;
+
+    Vector3 velocity;
+
+    [SerializeField]
+    private Transform groundCheck;
+    private float groundDistance = 0.4f;
+    [SerializeField]
+    private LayerMask groundMask;
+    public bool IsGrounded { get; set; }
+
+    private void Awake()
+    {
+        controller = this.GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        IsGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if(IsGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 move = (transform.right * x + transform.forward * z);
+        controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && IsGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+    }
+}
