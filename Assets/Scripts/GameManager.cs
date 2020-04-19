2b0f197d9e42ea299896550689aa6a14eb566ac4
Hugo <bombix34@gmail.com>
2020-04-20 01:03:16 +0200

    Ajout trigger load next zone

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
+    public List<GameObject> modulesInOrder;
     public Material okMaterial;
     public Material notOkMaterial;
 
     public PlayerSettings settings;
+
+    private int curIndexModule = -1;
 
     [Range(0f,100f)]
     public float currentLife = 100f;
@@ -15,6 +18,18 @@ public class GameManager : Singleton<GameManager>
     private void Awake()
     {
         currentLife = settings.decrepitTime;
+    }
+
+    private void Start()
+    {
+        if(modulesInOrder!=null)
+        {
+            for(int i =0; i < modulesInOrder.Count; ++i)
+            {
+                modulesInOrder[i].SetActive(false);
+            }
+            LoadNextModule();
+        }
     }
 
     private void Update()
@@ -29,6 +44,25 @@ public class GameManager : Singleton<GameManager>
         {
             currentLife = settings.decrepitTime;
         }
+    }
+
+    public void LoadNextModule()
+    {
+        if (modulesInOrder == null)
+        {
+            return;
+        }
+        curIndexModule++;
+        if(curIndexModule>=modulesInOrder.Count)
+        {
+            curIndexModule = modulesInOrder.Count-1;
+            return;
+        }
+        if(curIndexModule>=2)
+        {
+            modulesInOrder[curIndexModule - 2].SetActive(false);
+        }
+        modulesInOrder[curIndexModule].SetActive(true);
     }
 
 }
