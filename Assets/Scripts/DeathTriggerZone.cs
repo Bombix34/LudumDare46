4b6be205f45f0637e10c5719ee94a3bf00ff4a4c
Hugo <bombix34@gmail.com>
2020-04-19 23:47:11 +0200

    system de fly et plante a feed

--- Assets/Scripts/DeathTriggerZone.cs
+++ Assets/Scripts/DeathTriggerZone.cs
@@ -21,10 +21,6 @@ public class DeathTriggerZone :Singleton<DeathTriggerZone>
     {
         float yDisplacement = player.GetComponent<PlayerMovement>().Settings.deathTriggerZonePositionY;
         Vector3 finalPosition = new Vector3(player.transform.position.x, player.transform.position.y + yDisplacement, player.transform.position.z);
-        if(finalPosition.y == this.transform.position.y)
-        {
-            return;
-        }
         this.transform.position = finalPosition;
     }
 }
