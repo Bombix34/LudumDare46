4d6856b0e9a57c90b32bae5a6615258e6ba47b6d
Hugo <bombix34@gmail.com>
2020-04-20 22:36:09 +0200

    add audio event

---
+++ Assets/AudioEvent/Contents/PitchVolumeAudio.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName="FafaTools/Audio/PitchVolumeAudio")]
+public class PitchVolumeAudio : AudioEvent {
+	public RangedFloat volume;
+
+	[MinMaxRange(0,2)]
+	public RangedFloat pitch;
+	public override void Play(AudioSource source)
+    {
+        AudioClip toPlay = clips[Random.Range(0, clips.Length)];
+        source.outputAudioMixerGroup = audioMixerGroup;
+        source.volume = Random.Range(volume.minValue, volume.maxValue);
+        source.pitch = Random.Range(pitch.minValue, pitch.maxValue);
+        if (loop)
+        {
+            source.loop = loop;
+            source.clip = toPlay;
+            source.Play();
+        }
+        else
+        {
+            source.loop = false;
+            source.PlayOneShot(toPlay);
+        }
+    }
+}
